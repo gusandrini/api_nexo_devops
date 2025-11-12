94002df8bb5460a024994f25a129fed60c1f8d07
dudatiemiak <rm554756@fiap.com.br>
2025-11-11 21:37:13 -0300

    Iniciando API Java Nexo

---
+++ src/main/java/br/com/nexo/ApiNexoApplication.java
@@ -0,0 +1,13 @@
+package br.com.nexo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNexoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNexoApplication.class, args);
+	}
+
+}
