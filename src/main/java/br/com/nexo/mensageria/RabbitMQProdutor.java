74123baf8e898c6f9c40913523831bf18dbb5c63
gusandrini <gustavosandrini92@gmail.com>
2025-11-22 16:54:28 -0300

    ajsute rabit

--- src/main/java/br/com/nexo/mensageria/RabbitMQProdutor.java
+++ src/main/java/br/com/nexo/mensageria/RabbitMQProdutor.java
@@ -1,5 +1,8 @@
 package br.com.nexo.mensageria;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -7,12 +10,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class RabbitMQProdutor {
 
-	@Autowired
-	private RabbitTemplate rabbitTemplate;
-	
-	public void enviarMensagem(String mensagem) {
-		rabbitTemplate.convertAndSend(RabbitMQConfig.ROTEADOR, RabbitMQConfig.CHAVE_ROTA,mensagem);
-		System.out.println("Enviando mensagem via RabbitMQ: " + mensagem);
-	}
-	
+    private static final Logger logger = LoggerFactory.getLogger(RabbitMQProdutor.class);
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void enviarMensagem(String mensagem) {
+
+        try {
+            rabbitTemplate.convertAndSend(
+                    RabbitMQConfig.ROTEADOR,
+                    RabbitMQConfig.CHAVE_ROTA,
+                    mensagem
+            );
+
+            logger.info("Enviando mensagem via RabbitMQ: {}", mensagem);
+
+        } catch (AmqpException e) {
+            logger.warn("RabbitMQ indisponível. Mensagem NÃO enviada. Motivo: {}", e.getMessage());
+
+        } catch (Exception e) {
+            logger.warn("Falha inesperada ao enviar mensagem ao RabbitMQ. Ignorando. Motivo: {}", e.getMessage());
+        }
+    }
 }
